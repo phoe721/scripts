814bc526ac630f73d75f57ff1739b41eeaca5c30
Aaron Lin <phoe721@gmail.com>
2019-10-19 13:37:16 +0800

    Update

--- shell_script/backup_db.sh
+++ shell_script/backup_db.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 login_file=/root/secret/mylogin.cnf
-backup_file=/root/db/db_backup-$(date +%m%d%Y%H%M).sql.gz
-db_folder=1XWkVQn8Qxj8m5Ojfv1CkjG0W4rE5Gd08
+backup_file=/home/aaron/db/db_backup-$(date +%m%d%Y%H%M).sql.gz
+#db_folder=1XWkVQn8Qxj8m5Ojfv1CkjG0W4rE5Gd08
+db_folder=1Z7nTBLv5pHxGOCQLvhgZxMjpdAC4Kjp3
 echo "Backuping database..."
 /usr/bin/mysqldump --defaults-extra-file=$login_file -u root --single-transaction --quick --lock-tables=false 1perfectchoice | gzip > $backup_file
 echo "Uploading to Google Drive..."
